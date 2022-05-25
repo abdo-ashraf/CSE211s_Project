e281d98717c3fc177358dc9b05726bc746c15df1
Mohamedehab162001 <104224770+Mohamedehab162001@users.noreply.github.com>
2022-05-25 18:38:06 +0200

    Update main.c

--- main.c
+++ main.c
@@ -3,16 +3,16 @@ bool Is_Open_Door = false; //if false the door is closed if ture the door is ope
 
 int main()
 {
-unsigned char key; // used to get pressed key
-keypad_init();     //keypad initialization part
-LCD_init();        //lcd initialization part
-while(1){            //lcd main page 
-	LCD_command(clear_display);  //clear display
+unsigned char key;                       // used to get pressed key
+keypad_init();                           //keypad initialization part
+LCD_init();                              //lcd initialization part
+while(1){                                //lcd main page 
+	LCD_command(clear_display);      //clear display
 	LCD_string("Enter a choice");
 	LCD_command(CURSOR_ON_2ND_LINE);
 	LCD_string("to begin cooking");
 	delay_ms(250);
-	key = keypad_scan();	// scan for pressed key by user
+	key = keypad_scan();	         // scan for pressed key by user
 	switch(key){
 		case 'A':
 	        	popcorn();
